d3a53554c49cec10d763b8b7b52b4b0e19a980ad
Alirezat01 <tajvidinejad@gmail.com>
2026-08-18 10:26:13 +0330

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         method: 'POST',
         headers: { Authorization: `Bearer ${resendKey}`, 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          from: 'Karban <onboarding@resend.dev>',
+          from: 'Karban <no-reply@karbanapp.ir>',
           to: [to],
           subject,
           text: text || subject,
